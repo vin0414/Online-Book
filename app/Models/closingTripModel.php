3a3962b63d9d0a18b71a98e67b3d8589484bdc75
vin0414 <vinmogate@gmail.com>
2025-03-10 10:29:19 +0800

    add models

---
+++ app/Models/closingTripModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class closingTripModel extends Model
+{
+    use HasFactory;
+    protected $table = "tblclosetrip";
+    protected $primaryKey = "closeID";
+    protected $fillable = ['portID','tripID','fromDate','toDate','duration'];
+}
--- app/Models/logModel.php
+++ app/Models/logModel.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class logModel extends Model
 {
-    //
+    use HasFactory;
+    protected $table = "tblsystemlog";
+    protected $primaryKey = "logID";
+    protected $fillable = ['Date','userID','Activity','page'];
 }
